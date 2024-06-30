e4e38ff467837a88f1a6cb45e2b1d04147713f1f
Yanguangchen <yanguangchen@outlook.com>
2024-06-30 23:17:17 +0800

    changes to SEO

--- src/contactinfo.js
+++ src/contactinfo.js
@@ -37,8 +37,8 @@ function ContactInfo() {
         <div>
           <p className="pstyle">Phone: 6440 5131</p>
           <p className="pstyle">Email: richard@lemotor.com.sg</p>
-          <p className="pstyle">Desmond Tan (Sales) 9272 1418</p>
-          <p className="pstyle">Alan Sim(Purchases) 9821 1739</p>
+          <p className="pstyle">Richard Wong (Sales) 9769 2303</p>
+          <p className="pstyle">Alan Sim(Purchaser) 9821 1739</p>
           <video style={videoStyle}>
             <source
               muted
