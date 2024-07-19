75d151bbe3fdfe4ce6cd87e39ce99df1cc340c4e
Chen yg <yanguangchen@outlook.com>
2024-07-19 13:29:22 +0800

    c

--- src/splice.js
+++ src/splice.js
@@ -1,47 +1,28 @@
 import React from "react";
+import './splice.css';
 import SplineElement from "./splineElement";
 import darkModeVideo from "./Assets/darkModeCanvas.mp4";
-const containerWrapper = {
-  marginTop: "3%",
-  display: "flex",
-  justifyContent: "center",
-  alignItems: "center",
-  width: "100%",
-};
-
-const splineContainer = {
-  display: "flex",
-  justifyContent: "center",
-  alignItems: "center",
-  width: "100%",
-};
-
-const videoContainer = {
-  overflow: "hidden",
-  width: "100%",
-  display: "none"
-}
-
-
-const videoStyle = {
-  overflow: "hidden",
-  width: "100%",
-}
-
 
 function SpliceElement() {
   return (
     <div>
-      <div style={containerWrapper}>
-        <div style={splineContainer} className="splineContainer">
+      <div className="container-wrapper">
+        <div className="spline-container splineContainer">
           <div className="darkmodeContainer">
           </div>
           <SplineElement />
         </div>
       </div>
 
-      <div style={videoContainer} className="videoContainer">
-        <video src={darkModeVideo} style={videoStyle} muted autoPlay playsInline loop />
+      <div className="video-container videoContainer">
+        <video 
+          src={darkModeVideo} 
+          className="video-style" 
+          muted 
+          autoPlay 
+          playsInline 
+          loop 
+        />
       </div>
     </div>
   );
