154e401d408587eee057dc5dfc20dd8b5a0ac0dc
Chen yg <83961207+Yanguangchen@users.noreply.github.com>
2024-11-12 15:27:49 +0800

    Remove unused sitemap file and update quote and splice components for improved structure and readability

--- src/quote.js
+++ src/quote.js
@@ -1,1 +1,16 @@
-import React from 'react'; import './quote.css'; import quoteImage from "./Assets/quote.png"; function Quote() { React.useEffect(() => { const img = new Image(); img.src = quoteImage; }, []); return ( <> <link rel="preload" as="image" href={quoteImage} /> <div className="quote-container"> <img src={quoteImage} className="quote-image" alt="quote" /> </div> </> ); } export default Quote;
+import React from "react";
+import "./quote.css";
+import quoteImage from "./Assets/Quote.png";
+function Quote() {
+  return (
+    <>
+      {" "}
+      <link rel="preload" as="image" href={quoteImage} />{" "}
+      <div className="quote-container">
+        {" "}
+        <img src={quoteImage} className="quote-image" alt="quote" />{" "}
+      </div>{" "}
+    </>
+  );
+}
+export default Quote;
